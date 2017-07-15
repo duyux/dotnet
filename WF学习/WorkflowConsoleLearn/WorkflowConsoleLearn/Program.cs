ec54cbd3c9f049e35635be0fdeb62727a1df7670
duyux <duyux@163.com>
2017-07-15 13:21:36 +0800

    wf

---
+++ WF学习/WorkflowConsoleLearn/WorkflowConsoleLearn/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+using System.Collections.Generic;
+
+namespace WorkflowConsoleLearn
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Activity workflow1 = new Workflow1();
+
+            var wfArg = new Dictionary<string, Object>();
+            wfArg.Add("argInAge", 39);
+
+            WorkflowInvoker.Invoke(workflow1,wfArg);
+            Console.ReadKey();
+        }
+    }
+}
